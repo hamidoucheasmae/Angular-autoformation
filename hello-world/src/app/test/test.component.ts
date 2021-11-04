9888e02be4f98140e4e37d2008de2af6154d9a42
hamidoucheasmae <76485278+hamidoucheasmae@users.noreply.github.com>
2021-11-04 12:05:50 +0100

    component test

--- hello-world/src/app/test/test.component.ts
+++ hello-world/src/app/test/test.component.ts
@@ -2,8 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+  template: `
+ <h1>INLINE TEMPLATE</h1>
+    
+  `,
+  styles: ['h1 { color: red; }']
 })
 export class TestComponent implements OnInit {
 
