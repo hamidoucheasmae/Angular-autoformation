9117cc934c128c39528310429fba5d3e9037ab1a
hamidoucheasmae <76485278+hamidoucheasmae@users.noreply.github.com>
2021-11-04 16:30:29 +0100

    update property binding

--- binding/src/app/test/test.component.ts
+++ binding/src/app/test/test.component.ts
@@ -9,12 +9,18 @@ import { Component, OnInit } from '@angular/core';
   <h2>{{name.toUpperCase()}} </h2>
   <h2>{{greetUser()}} </h2>
   <h2>{{siteUrl}} </h2>
+  <input [id]="myId" type="text" value="mid"> 
+  <input [disabled]="true" id="{{myId}}" type="text" value="mid"> 
+  <input [disabled]="isDisabled" id="{{myId}}" type="text" value="mind"> 
+  
   `,
  styles:[]
 })
 export class TestComponent implements OnInit {
  public siteUrl =window.location.href;
   public name= "asmae ham";
+  public isDisabled =true;
+  public myId ="testId";
   constructor() { }
 
   ngOnInit(): void {
